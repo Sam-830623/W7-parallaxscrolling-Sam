c2e2350948aebd5e35be05327c26fb037cdc4381
Sam <annie24791541@gmail.com>
2021-08-16 23:32:22 +0800

    Update 2021-08-16T15:32:03.296Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,14 +1,11 @@
+"use strict";
+
 console.log('Hello!');
-
-$(document).ready(() => {
+$(document).ready(function () {
   console.log('HesSchool Hello!');
-});
+}); // aos
 
-// aos
-AOS.init();
-
-// 以下是可以各別套用其他東西嗎?
-
+AOS.init(); // 以下是可以各別套用其他東西嗎?
 // AOS.init({
 //   // Global settings:
 //   disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
@@ -19,8 +16,6 @@ AOS.init();
 //   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
 //   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
 //   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-  
-
 //   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
 //   offset: 120, // offset (in px) from the original trigger point
 //   delay: 0, // values from 0 to 3000, with step 50ms
@@ -29,5 +24,5 @@ AOS.init();
 //   once: false, // whether animation should happen only once - while scrolling down
 //   mirror: false, // whether elements should animate out while scrolling past them
 //   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
 // });
+//# sourceMappingURL=all.js.map
